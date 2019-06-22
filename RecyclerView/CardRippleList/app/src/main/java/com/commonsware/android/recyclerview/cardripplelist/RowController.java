cea6f54db615de26989ac990586877e9f79985db
Kaiyin Zhong <k.zhong@gemvision.io>
2019-06-23 00:50:48 +0200

    ripple on card

--- RecyclerView/CardRippleList/app/src/main/java/com/commonsware/android/recyclerview/cardripplelist/RowController.java
+++ RecyclerView/CardRippleList/app/src/main/java/com/commonsware/android/recyclerview/cardripplelist/RowController.java
@@ -21,40 +21,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 class RowController extends RecyclerView.ViewHolder
-    implements View.OnClickListener {
-  TextView label=null;
-  TextView size=null;
-  ImageView icon=null;
-  String template=null;
+        implements View.OnClickListener {
+    TextView label = null;
+    TextView size = null;
+    ImageView icon = null;
+    String template = null;
 
-  RowController(View row) {
-    super(row);
+    RowController(View row) {
+        super(row);
 
-    label=(TextView)row.findViewById(R.id.label);
-    size=(TextView)row.findViewById(R.id.size);
-    icon=(ImageView)row.findViewById(R.id.icon);
+        label = (TextView) row.findViewById(R.id.label);
+        size = (TextView) row.findViewById(R.id.size);
+        icon = (ImageView) row.findViewById(R.id.icon);
 
-    template=size.getContext().getString(R.string.size_template);
+        template = size.getContext().getString(R.string.size_template);
 
-    row.setOnClickListener(this);
-  }
-
-  @Override
-  public void onClick(View v) {
-    Toast.makeText(v.getContext(),
-        String.format("Clicked on position %d", getAdapterPosition()),
-        Toast.LENGTH_SHORT).show();
-  }
-
-  void bindModel(String item) {
-    label.setText(item);
-    size.setText(String.format(template, item.length()));
-
-    if (item.length()>4) {
-      icon.setImageResource(R.drawable.delete);
+        row.setOnClickListener(this);
     }
-    else {
-      icon.setImageResource(R.drawable.ok);
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(v.getContext(),
+                String.format("Clicked on position %d", getAdapterPosition()),
+                Toast.LENGTH_SHORT).show();
     }
-  }
+
+    void bindModel(String item) {
+        label.setText(item);
+        size.setText(String.format(template, item.length()));
+
+        if (item.length() > 4) {
+            icon.setImageResource(R.drawable.delete);
+        } else {
+            icon.setImageResource(R.drawable.ok);
+        }
+    }
 }
