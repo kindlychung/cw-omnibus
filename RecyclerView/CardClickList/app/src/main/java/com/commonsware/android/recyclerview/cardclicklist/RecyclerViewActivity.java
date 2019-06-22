cf97ad0c11e21121e6082873394ca5654b1a801d
Kaiyin Zhong <k.zhong@gemvision.io>
2019-06-23 00:46:24 +0200

    clickable card

--- RecyclerView/CardClickList/app/src/main/java/com/commonsware/android/recyclerview/cardclicklist/RecyclerViewActivity.java
+++ RecyclerView/CardClickList/app/src/main/java/com/commonsware/android/recyclerview/cardclicklist/RecyclerViewActivity.java
@@ -18,27 +18,27 @@ import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 
 public class RecyclerViewActivity extends Activity {
-  private RecyclerView rv=null;
+    private RecyclerView rv = null;
 
-  public void setAdapter(RecyclerView.Adapter adapter) {
-    getRecyclerView().setAdapter(adapter);
-  }
-
-  public RecyclerView.Adapter getAdapter() {
-    return(getRecyclerView().getAdapter());
-  }
-
-  public void setLayoutManager(RecyclerView.LayoutManager mgr) {
-    getRecyclerView().setLayoutManager(mgr);
-  }
-
-  public RecyclerView getRecyclerView() {
-    if (rv==null) {
-      rv=new RecyclerView(this);
-      rv.setHasFixedSize(true);
-      setContentView(rv);
+    public void setAdapter(RecyclerView.Adapter adapter) {
+        getRecyclerView().setAdapter(adapter);
     }
 
-    return(rv);
-  }
+    public RecyclerView.Adapter getAdapter() {
+        return (getRecyclerView().getAdapter());
+    }
+
+    public void setLayoutManager(RecyclerView.LayoutManager mgr) {
+        getRecyclerView().setLayoutManager(mgr);
+    }
+
+    public RecyclerView getRecyclerView() {
+        if (rv == null) {
+            rv = new RecyclerView(this);
+            rv.setHasFixedSize(true);
+            setContentView(rv);
+        }
+
+        return (rv);
+    }
 }
