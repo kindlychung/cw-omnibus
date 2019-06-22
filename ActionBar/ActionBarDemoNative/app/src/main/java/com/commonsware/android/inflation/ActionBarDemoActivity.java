4391814a7fe5e23df849f28cc7860737a15d27ca
Kaiyin Zhong <k.zhong@gemvision.io>
2019-06-23 01:18:36 +0200

    update

--- ActionBar/ActionBarDemoNative/app/src/main/java/com/commonsware/android/inflation/ActionBarDemoActivity.java
+++ ActionBar/ActionBarDemoNative/app/src/main/java/com/commonsware/android/inflation/ActionBarDemoActivity.java
@@ -1,15 +1,15 @@
 /***
-  Copyright (c) 2008-2012 CommonsWare, LLC
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-  use this file except in compliance with the License. You may obtain	a copy
-  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
-  by applicable law or agreed to in writing, software distributed under the
-  License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
-  OF ANY KIND, either express or implied. See the License for the specific
-  language governing permissions and limitations under the License.
-  
-  Covered in detail in the book _The Busy Coder's Guide to Android Development_
-    https://commonsware.com/Android
+ Copyright (c) 2008-2012 CommonsWare, LLC
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain	a copy
+ of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ by applicable law or agreed to in writing, software distributed under the
+ License is distributed on an "AS IS" BASIS,	WITHOUT	WARRANTIES OR CONDITIONS
+ OF ANY KIND, either express or implied. See the License for the specific
+ language governing permissions and limitations under the License.
+
+ Covered in detail in the book _The Busy Coder's Guide to Android Development_
+ https://commonsware.com/Android
  */
 
 package com.commonsware.android.inflation;
@@ -24,72 +24,66 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class ActionBarDemoActivity extends ListActivity {
-  private static final String[] items= { "lorem", "ipsum", "dolor",
-      "sit", "amet", "consectetuer", "adipiscing", "elit", "morbi",
-      "vel", "ligula", "vitae", "arcu", "aliquet", "mollis", "etiam",
-      "vel", "erat", "placerat", "ante", "porttitor", "sodales",
-      "pellentesque", "augue", "purus" };
-  private ArrayList<String> words=null;
-  private ArrayAdapter<String> adapter=null;
+    private static final String[] items = {"lorem", "ipsum", "dolor",
+            "sit", "amet", "consectetuer", "adipiscing", "elit", "morbi",
+            "vel", "ligula", "vitae", "arcu", "aliquet", "mollis", "etiam",
+            "vel", "erat", "placerat", "ante", "porttitor", "sodales",
+            "pellentesque", "augue", "purus"};
+    private ArrayList<String> words = null;
+    private ArrayAdapter<String> adapter = null;
 
-  @Override
-  public void onCreate(Bundle state) {
-    super.onCreate(state);
-
-    initAdapter();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.actions, menu);
-
-    return(super.onCreateOptionsMenu(menu));
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch(item.getItemId()) {
-      case R.id.add:
-        addWord();
-
-        return(true);
-
-      case R.id.reset:
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
         initAdapter();
-
-        return(true);
-
-      case R.id.about:
-        Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG)
-             .show();
-
-        return(true);
     }
 
-    return(super.onOptionsItemSelected(item));
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
 
-  private void initAdapter() {
-    words=new ArrayList<String>();
-
-    for (int i=0;i<5;i++) {
-      words.add(items[i]);
+        return (super.onCreateOptionsMenu(menu));
     }
 
-    adapter=
-        new ArrayAdapter<String>(this,
-                                 android.R.layout.simple_list_item_1,
-                                 words);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                addWord();
+                return (true);
 
-    setListAdapter(adapter);
-  }
+            case R.id.reset:
+                initAdapter();
+                return (true);
 
-  private void addWord() {
-    if (adapter.getCount()<items.length) {
-      adapter.add(items[adapter.getCount()]);
+            case R.id.about:
+                Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG)
+                        .show();
+                return (true);
+        }
+
+        return (super.onOptionsItemSelected(item));
     }
-  }
+
+    private void initAdapter() {
+        words = new ArrayList<String>();
+        for (int i = 0; i < 5; i++) {
+            words.add(items[i]);
+        }
+        adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        words);
+        setListAdapter(adapter);
+    }
+
+    private void addWord() {
+        if (adapter.getCount() < items.length) {
+            adapter.add(items[adapter.getCount()]);
+        }
+    }
 }
